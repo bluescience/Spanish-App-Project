50d7882d413990b4586aa56289896c39b28c7a66
Ronald Longcape <Ronald Longcape>
2017-12-20 17:36:11 -0800

    Working Filter Commit

--- src/filter.js
+++ src/filter.js
@@ -1,54 +1,52 @@
-var ulDiv = document.getElementById('ulDiv');
 
 
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
 
-`
+function drawCheckboxes(){
+	var ulDiv = document.getElementById('ulDiv');
+	ulDiv.innerHTML += 'PLEASE SELECT THE TENSES YOU WISH TO PRACTICE';
+	var ulElement = document.createElement("ul");
+	ulElement.setAttribute('id', 'tensesFilter');
+	for(x in filterArray){
+		var liElement = document.createElement("li");
+		var checkboxElement = document.createElement("input");
+		checkboxElement.setAttribute('type', 'checkBox');
+		checkboxElement.setAttribute('id', filterArray[x]);
+		liElement.appendChild(checkboxElement);
+		liElement.innerHTML += capitalizeFirstLetter(filterArray[x].replace(/([A-Z])/g, ' $1')).trim();
+		ulElement.appendChild(liElement)
+	
+	}
+	
+	ulDiv.appendChild(ulElement);
+	filterClicker();
+}
 
-<ul id = 'tensesFilter'>
+window.onload = drawCheckboxes();
 
-<li> <input type ="checkBox" id = 'conditionalContinuous'  > Conditional Continuous </li>
-<li> <input type ="checkBox" id = 'conditionalIndicative'  > Conditional Indicative </li>
-<li> <input type ="checkBox" id = 'conditionalPerfect'  > Conditional Perfect </li>
-<li> <input type ="checkBox" id = 'futureContinuous'  > Future Continuous </li>
-<li> <input type ="checkBox" id = 'futureIndicative'  > Future Indicative </li>
-<li> <input type ="checkBox" id = 'futurePerfect'  > Future Perfect </li>
-<li> <input type ="checkBox" id = 'futurePerfectSubjunctive'  > Future Perfect Subjunctive </li>
-<li> <input type ="checkBox" id = 'futureSubjunctive'  > Future Subjunctive </li>
-<li> <input type ="checkBox" id = 'imperative'  > Imperative </li>
-<li> <input type ="checkBox" id = 'imperfectContinous'  > Imperfect Continous </li>
-<li> <input type ="checkBox" id = 'imperfectIndicative'  > Imperfect Indicative </li>
-<li> <input type ="checkBox" id = 'imperfectSubjunctive'  > Imperfect Subjunctive </li>
-<li> <input type ="checkBox" id = 'imperfectSubjunctive2'  > Old Imperfect Subjunctive </li>
-<li> <input type ="checkBox" id = 'negativeImperfect'  > Negative Imperfect </li>
-<li> <input type ="checkBox" id = 'pastPerfect'  > Past Perfect </li>
-<li> <input type ="checkBox" id = 'pastPerfectSubjunctive'  > Past Perfect Subjunctive </li>
-<li> <input type ="checkBox" id = 'presentContinuous'  > Present Continuous </li>
-<li> <input type ="checkBox" id = 'presentIndicative'  > Present Indicative </li>
-<li> <input type ="checkBox" id = 'presentPerfect'  > Present Perfect </li>
-<li> <input type ="checkBox" id = 'presentPerfectSubjunctive'  > Present Perfect Subjunctive</li>
-<li> <input type ="checkBox" id = 'presentSubjunctive'  > Present Subjunctive </li>
-<li> <input type ="checkBox" id = 'preteritContinuous'  > Preterit Continuous </li>
-<li> <input type ="checkBox" id = 'preteritIndicative'  > Preterit Indicative </li>
-<li> <input type ="checkBox" id = 'preteritPerfect'  > Preterit Perfect </li>
+function filterClicker(){
+	var submitDiv = document.getElementById('submitDiv');
+	var submitBoxFilterForFilter = document.createElement('input');
+	submitBoxFilterForFilter.setAttribute('type', 'submit');
+	submitBoxFilterForFilter.setAttribute('value', 'Submit');
+	submitBoxFilterForFilter.setAttribute('id', 'submitBoxFilter');
+	submitDiv.appendChild(submitBoxFilterForFilter);
+	
+	checkboxes = $('ul li input[type=checkbox]');
 
-<br>
-<input type="submit" value="Submit" id="submitBoxFilter">
-</ul>`;
-
-
-checkboxes = $('ul li input[type=checkbox]')
-
-var tenseArray = []
-
-document.getElementById("submitBox").addEventListener("click", function(){
-	checkboxes.each(function(e){
-		if(this.checked == true){
-			tenseArray.push($(this).attr('id'));
-		}
+	document.getElementById("submitBoxFilter").addEventListener("click", function(){
+		checkboxes.each(function(e){
+			if(this.checked == true){
+				keyValueAllowedList.push($(this).attr('id'));
+			}
+		});
+		console.log(keyValueAllowedList)
+		drawTable();
 	});
-	location.href = 'index.html'
-});
 
+}
 
 
 
